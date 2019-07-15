e3fe0a369de405d48b257abb9faae652f456264a
Syed Muhammad Zaeem Hasan Kazmi <zaeemkazmi@dhcp-10-181-38-57.dynamic.eduroam.mwn.de>
2019-07-15 20:08:33 +0200

    Murat and Zaeem code merged for frontend

--- src/models/user.js
+++ src/models/user.js
@@ -67,8 +67,9 @@ const userSchema = new mongoose.Schema(
       }
     },
     image: {
-      type: String,
-      required: true
+      type: String
+      // ,
+      // required: true
     },
     countryOfOrigin: {
       type: String,
